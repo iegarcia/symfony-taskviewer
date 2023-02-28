d7f4c34eb458e15b65494164789c184830718c8b
Ignacio Garcia <nachogarcia003@gmail.com>
2023-02-27 22:28:07 -0300

    Add styles to form

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -6,16 +6,24 @@ use App\Entity\Task;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class TaskType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('title')
-            ->add('description')
-        ;
-    }
+{
+    $builder
+        ->add('title', TextType::class, [
+            'attr' => [
+                'class' => 'form-control'
+            ]
+        ])
+        ->add('description', TextType::class, [
+            'attr' => [
+                'class' => 'form-control'
+            ]
+        ]);
+}
 
     public function configureOptions(OptionsResolver $resolver)
     {
